a67365a38c17f5575aabea5c335524562b02d6ce
Maglione Riccardo <riccardo99maglione@gmail.com>
2020-11-27 11:37:58 +0100

    Add menu scene with map and add script for player

---
+++ Assets/Scripts/SceneScripts/DoorWarp.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorWarp : MonoBehaviour
+{
+    public GameObject Player1;
+    public GameObject Player2;
+    public GameObject Player3;
+    void Update()
+    {
+        WarpDoor();
+    }
+
+    public void WarpDoor()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider != null)
+            {
+                if (hit.collider.name == "Door1")
+                {
+                    MapWarp.Warp1 = true;
+                    MapWarp.Warp2 = false;
+                    MapWarp.Warp3 = false;
+                    Player1.SetActive(true);
+                    Player2.SetActive(false);
+                    Player3.SetActive(false);
+                }
+                if (hit.collider.name == "Door2")
+                {
+                    MapWarp.Warp1 = false;
+                    MapWarp.Warp2 = true;
+                    MapWarp.Warp3 = false;
+                    Player1.SetActive(false);
+                    Player2.SetActive(true);
+                    Player3.SetActive(false);
+                }
+                if (hit.collider.name == "Door3")
+                {
+                    MapWarp.Warp1 = false;
+                    MapWarp.Warp2 = false;
+                    MapWarp.Warp3 = true;
+                    Player1.SetActive(false);
+                    Player2.SetActive(false);
+                    Player3.SetActive(true);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SceneScripts/MapWarp.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapWarp : MonoBehaviour
+{
+    #region Variables
+    public GameObject MenuPanel;
+    public GameObject MapPanel;
+    public GameObject OptionPanel;
+
+    public static bool Warp1;
+    public static bool Warp2;
+    public static bool Warp3;
+    #endregion
+
+    #region Method of Scene Menu
+    /// <summary>
+    /// Scena Menu
+    /// Pulsante che disattiva i pannelli accesi e passa al pannello della mappa, da cui poi si andrà
+    /// alla scena di gioco
+    /// </summary>
+    public void Play()
+    {
+        MenuPanel.SetActive(false);
+        MapPanel.SetActive(true);
+    }
+
+    /// <summary>
+    /// Scena Menu
+    /// Metodo per tornare nel menu disattivando la schermata della mappa - Upgrade mettere tutte le schermate disattivate della scena corrispondente
+    /// </summary>
+    public void BackToMenu()
+    {
+        MapPanel.SetActive(false);
+        MenuPanel.SetActive(true);
+    }
+    #endregion
+
+    #region Method of Scene Gameplay
+    /// <summary>
+    /// Scena Gameplay
+    /// Metodo per tornare al gioco disattivando la schermata della mappa - Upgrade mettere tutte le schermate disattivate della scena corrispondente
+    /// </summary>
+    public void BackToPlay()
+    {
+        MapPanel.SetActive(false);
+        OptionPanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+    #endregion
+
+    #region Method of both scene
+    public void WarpPicture1()
+    {
+        Warp2 = false;
+        Warp3 = false;
+        Warp1 = true;
+        SceneManager.LoadScene("BlockOut");
+    }
+    public void WarpPicture2()
+    {
+        Warp1 = false;
+        Warp3 = false;
+        Warp2 = true;
+        SceneManager.LoadScene("BlockOut");
+    }
+    public void WarpPicture3()
+    {
+        Warp1 = false;
+        Warp2 = false;
+        Warp3 = true;
+        SceneManager.LoadScene("BlockOut");
+    }
+
+    #endregion
+}
