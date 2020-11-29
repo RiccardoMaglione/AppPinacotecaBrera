8d176621d57ec69466b4f8ebc49c111a3e494812
Maglione Riccardo <riccardo99maglione@gmail.com>
2020-11-29 22:35:00 +0100

    Adjust limit movement

--- Assets/Scripts/RotationScript/BaseRotation.cs
+++ Assets/Scripts/RotationScript/BaseRotation.cs
@@ -9,22 +9,23 @@ public class BaseRotation : MonoBehaviour
         transform.RotateAround(transform.position, transform.right, -axis * Time.deltaTime);
         #region LimitRange on X
         // get relative range +/-
-        float relRange = (80 - -80) / 2f;
+        float relRange = (40 - -40) / 2f;
 
         // calculate offset
-        float offset = 80 - relRange;
+        float offset = 40 - relRange;
 
         // convert to a relative value
         Vector3 angles = transform.eulerAngles;
         float x = ((angles.x + 540) % 360) - 180 - offset;
-
+        angles.z = 0;
         // if outside range
-        if (Mathf.Abs(x) > relRange)
+        if (Mathf.Abs(x) >= relRange)
         {
             angles.x = relRange * Mathf.Sign(x) + offset;
             transform.eulerAngles = angles;
         }
         #endregion
+
     }
 
     //rotate the camera rigt and left (y rotation)
