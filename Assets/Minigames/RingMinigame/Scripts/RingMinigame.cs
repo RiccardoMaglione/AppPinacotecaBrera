24cc6a2db44fe3de9b76fcec63f9f25429b7ff36
Grayson1509 <alessiomanna.job@gmail.com>
2020-11-29 17:05:36 +0100

    created minigame resources

---
+++ Assets/Minigames/RingMinigame/Scripts/RingMinigame.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+namespace Brera.Minigames.AssemblerPuzzle
+{
+    public sealed class RingMinigame : MonoBehaviour
+    {
+        public GameObject[] _pieces;
+        Quaternion[] rotations;
+
+        private void Start()
+        {
+            rotations = new Quaternion[_pieces.Length];
+
+            for(int i = 0; i < _pieces.Length; i++)
+            {
+                rotations[i] = _pieces[i].transform.rotation;
+            }
+
+            /* Randomize Rotations */
+            Quaternion[] rots = { Quaternion.Euler(0, 0, 45), Quaternion.Euler(0, 0, 90), Quaternion.Euler(0, 0, 180), Quaternion.Euler(0, 0, 270) };
+            foreach(var item in _pieces)
+            {
+                item.transform.rotation = rots[Random.Range(0, rots.Length)];
+            }
+
+
+        }
+        private void Update()
+        {
+            _pieces[0].GetComponent<SpriteRenderer>().color = _pieces[0].transform.rotation == Quaternion.Euler(0,0,0) || _pieces[0].transform.rotation == Quaternion.Euler(0,0,360) ? _pieces[0].GetComponent<SpriteRenderer>().color = Color.green : _pieces[0].GetComponent<SpriteRenderer>().color = Color.red;
+            _pieces[1].GetComponent<SpriteRenderer>().color = _pieces[1].transform.rotation == Quaternion.Euler(0,0,0) || _pieces[1].transform.rotation == Quaternion.Euler(0,0,360) ? _pieces[1].GetComponent<SpriteRenderer>().color = Color.green : _pieces[1].GetComponent<SpriteRenderer>().color = Color.red;
+            _pieces[2].GetComponent<SpriteRenderer>().color = _pieces[2].transform.rotation == Quaternion.Euler(0,0,0) || _pieces[2].transform.rotation == Quaternion.Euler(0,0,360) ? _pieces[2].GetComponent<SpriteRenderer>().color = Color.green : _pieces[2].GetComponent<SpriteRenderer>().color = Color.red;
+        }
+
+
+    }
+}
---
+++ Assets/Minigames/RingMinigame/Scripts/RingPiece.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+namespace Brera.Minigames.AssemblerPuzzle
+{
+    public class RingPiece : MonoBehaviour
+    {
+        Vector3 direction = Vector3.zero;
+        Vector3 _prevPos = Vector3.zero;
+        Vector3 _lastPos = Vector3.zero;
+        bool _canSnap = false;
+
+
+
+
+        private void OnMouseDown()
+        {
+            _canSnap = true;
+            _prevPos = Input.mousePosition;
+
+            //Debug.LogError("VERO");
+        }
+        public void OnMouseDrag()
+        {
+
+            if (_canSnap)
+            {
+                System.Threading.Thread.Sleep(500);
+                _lastPos = Input.mousePosition;
+
+                //Debug.LogError($"direction : {(_lastPos - _prevPos).normalized}");
+
+                if (_prevPos.x < _lastPos.x || _prevPos.y < _lastPos.y)
+                {
+                    RotateObject(-45);
+                    //Debug.LogWarning("LEFT");
+                    _canSnap = false;
+
+                }
+                else
+                {
+                    RotateObject(45);
+                    //Debug.LogWarning("RIGHT");
+                    _canSnap = false;
+
+                }
+            }
+        }
+
+        private void Update()
+        {
+            //if (Input.GetMouseButtonDown(0)) RotateObject(45);
+            //if (Input.GetMouseButtonDown(1)) RotateObject(-45);
+
+
+        }
+
+        void RotateObject(float degs) => gameObject.transform.rotation *= Quaternion.Euler(0, 0, degs);
+    }
+}
