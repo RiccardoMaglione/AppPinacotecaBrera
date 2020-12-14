388711707a2cd63633f3834ba4376600ca7d1b1c
Maglione Riccardo <riccardo99maglione@gmail.com>
2020-12-14 09:19:26 +0100

    Comment script for reset playerprefs

--- Assets/Scripts/SceneScripts/ResetPlayer.cs
+++ Assets/Scripts/SceneScripts/ResetPlayer.cs
@@ -1,14 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
-public class ResetPlayer : MonoBehaviour
-{
-    [MenuItem("Edit/Reset Peppino")]
-    static void ResetPrefs()
-    {
-        PlayerPrefs.SetInt("Luca", 0);
-        PlayerPrefs.SetInt("Puzzle", 0);
-        PlayerPrefs.SetInt("Light", 0);
-    }
-}
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEditor;
+//public class ResetPlayer : MonoBehaviour
+//{
+//    [MenuItem("Edit/Reset Peppino")]
+//    static void ResetPrefs()
+//    {
+//        PlayerPrefs.SetInt("Luca", 0);
+//        PlayerPrefs.SetInt("Puzzle", 0);
+//        PlayerPrefs.SetInt("Light", 0);
+//    }
+//}
