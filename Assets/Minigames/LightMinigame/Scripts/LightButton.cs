878d6bb4eed76747bc09561464381de9e3b6080e
Maglione Riccardo <riccardo99maglione@gmail.com>
2020-12-16 19:07:18 +0100

    Add script of change image to button of light in on and off

---
+++ Assets/Minigames/LightMinigame/Scripts/LightButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightButton : MonoBehaviour
+{
+    public GameObject Button;
+    public Sprite ImgOn;
+    public Sprite ImgOff;
+    int Count = 0;
+
+    public void ChangeImage()
+    {
+        if (Count == 0)
+        {
+            Button.GetComponent<Image>().sprite = ImgOn;
+            Count++;
+        }
+        else
+        {
+            Button.GetComponent<Image>().sprite = ImgOff;
+            Count = 0;
+        }
+    }
+}
