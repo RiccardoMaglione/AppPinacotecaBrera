7cc301ee814dd4310a62790c560ca4f68c0d0fbc
LucaDL <lanzellottiluca9800@gmail.com>
2020-12-18 18:11:56 +0100

    Add Sound BackGround

---
+++ Assets/Cartella_Luca/Scripts/StartSound2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSound2 : MonoBehaviour
+{
+    [SerializeField]
+    public string Suono;
+
+    [SerializeField]
+    public string SuonoOFF;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.instance.Play(Suono);
+        AudioManager.instance.Stop(SuonoOFF);
+    }
+
+   
+}
