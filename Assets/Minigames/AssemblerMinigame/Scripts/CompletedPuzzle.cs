0d3bd04fde8d31b9f29ccaaebe6355df24e0a3af
Joe Abiad <Joenielscrs@gmail.com>
2020-12-19 20:51:06 +0100

    Third Minigame - Fix References

--- Assets/Minigames/AssemblerMinigame/Scripts/CompletedPuzzle.cs
+++ Assets/Minigames/AssemblerMinigame/Scripts/CompletedPuzzle.cs
@@ -6,7 +6,7 @@ public class CompletedPuzzle : MonoBehaviour
 {
     public GameObject[] DeactiveObject;
 
-    void Start()
+    void Update()
     {
         if(PlayerPrefs.GetInt("PuzzleCompleted") == 1)
         {
