d176f2e631b5f1d732cb1a64fc9d6d3878d50051
Joe Abiad <Joenielscrs@gmail.com>
2020-12-20 17:48:40 +0100

    UI - Stats references Setted / UI - MapGeneral Anchor&Pivot Updated

--- Assets/Scripts/SceneScripts/ResetPlayer.cs
+++ Assets/Scripts/SceneScripts/ResetPlayer.cs
@@ -1,24 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
-public class ResetPlayer : MonoBehaviour
-{
-    [MenuItem("Edit/Reset Peppino")]
-    static void ResetPrefs()
-    {
-        PlayerPrefs.SetString("Nickname", "Player's Name");
-        PlayerPrefs.SetInt("NicknameControl", 0);
-        PlayerPrefs.SetInt("Luca", 0);
-        PlayerPrefs.SetInt("Puzzle", 0);
-        PlayerPrefs.SetInt("Light", 0);
-        PlayerPrefs.SetInt("PuzzleCompleted", 0);
-        PlayerPrefs.SetInt("Ring", 0);
-        PlayerPrefs.SetInt("LightDoor", 0);
-        PlayerPrefs.SetInt("BadgeFinal", 0);
-        PlayerPrefs.SetInt("FirstBadge", 0);
-        PlayerPrefs.SetInt("RingImage", 0);
-        PlayerPrefs.SetInt("LightImage", 0);
-        PlayerPrefs.SetInt("PuzzleImage", 0);
-    }
-}
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEditor;
+//public class ResetPlayer : MonoBehaviour
+//{
+//    [MenuItem("Edit/Reset Peppino")]
+//    static void ResetPrefs()
+//    {
+//        PlayerPrefs.SetString("Nickname", "Player's Name");
+//        PlayerPrefs.SetInt("NicknameControl", 0);
+//        PlayerPrefs.SetInt("Luca", 0);
+//        PlayerPrefs.SetInt("Puzzle", 0);
+//        PlayerPrefs.SetInt("Light", 0);
+//        PlayerPrefs.SetInt("PuzzleCompleted", 0);
+//        PlayerPrefs.SetInt("Ring", 0);
+//        PlayerPrefs.SetInt("LightDoor", 0);
+//        PlayerPrefs.SetInt("BadgeFinal", 0);
+//        PlayerPrefs.SetInt("FirstBadge", 0);
+//        PlayerPrefs.SetInt("RingImage", 0);
+//        PlayerPrefs.SetInt("LightImage", 0);
+//        PlayerPrefs.SetInt("PuzzleImage", 0);
+//    }
+//}
